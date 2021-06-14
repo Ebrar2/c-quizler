899ad664f5686b7bf8249eb3f74ef057f1b91439
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 16:20:35 +0300

    Girilen yarıçap bilgisine göre çemberin çevresini alnını bulma

--- main.c
+++ main.c
@@ -1,62 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<time.h>
+float getCap(float r);
+float getCemberCevre(float r,float pi);
+float getCemberAlan(float r,float pi);
 int main()
 {
-    int d[10],i,j,gecici,c=0,t=0;
-    printf("Rastgele dizi:\n");
-    srand(time(NULL));
-    for(i=0;i<10;i++)
-    {
-    d[i]=rand()%101;
-     printf("%d,",d[i]);
-    }
-    for(i=0;i<10;i++)
-    {
-        for(j=i+1;j<10;j++)
-          {
-         if(d[i]>d[j])
-           {
-           gecici=d[j];
-           d[j]=d[i];
-           d[i]=gecici;
-           }
-          }
-    }
-      printf("\n\nSirali dizi:");
-      for(i=0;i<10;i++)
-       {
-       printf("%d,",d[i]);}
-
-       printf("\n\n");
-       printf("Once tek sonra cift sayilar sirali dizi\n");
-    for(i=0;i<10;i++)
-  {
-   if(d[i]%2==0)
-    c++;
-    else
-    t++;
-
-}
-   int cift[c],tek[t];
-    int a=0,b=0;
-   for(i=0;i<10;i++)
-   {
-   if(d[i]%2==0)
-    {
-   cift[a]=d[i];
-   a++;
-  }
-   else
-   {
-   tek[b]=d[i];
-    b++;
-   }
-   }
-  for(int i=0;i<t;i++)
-  printf("%d,",tek[i]);
-   for(int i=0;i<c;i++)
-   printf("%d,",cift[i]);
+    float pi=3.14;
+    float r;
+    printf("Cemberin yaricapini giriniz:");
+    scanf("%f",&r);
+    printf("\nCemberin capi:%.2f",getCap(r));
+    printf("\nCemberin cevresi:%.2f",getCemberCevre(r,pi));
+    printf("\nCemberin alani:%.2f",getCemberAlan(r,pi));
 
     return 0;
+}
+float getCap(float r)
+{
+    float cap;
+    cap=2*r;
+    return cap;
+}
+float getCemberCevre(float r,float pi)
+{
+    float cevre;
+    cevre=2*pi*r;
+    return cevre;
+}
+float getCemberAlan(float r,float pi)
+{
+    float alan;
+    alan=pi*r*r;
+    return alan;
 }
