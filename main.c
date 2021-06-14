1bc22e696850f28806e9f804372cfe0b328f84ed
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 15:58:36 +0300

    Girilen iki sayı arasındaki asal sayıları bulma

--- main.c
+++ main.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 //EBRAR TEMEL 200201085
-void xParseSeconds (int seconds);
+
 int main()
 {
-    int saniye;
-    printf("Saniye bilgisini giriniz:");
-    scanf("%d",&saniye);
-    xParseSeconds(saniye);
+    int ilk,son,a,b,kontrol=0;
+    printf("Ilk deger:");
+    scanf("%d",&ilk);
+    printf("Son deger:");
+    scanf("%d",&son);
+    if(ilk>son){
+    printf("Asal sayilar %d ile %d arasindaki:",son,ilk);
+    for(a=ilk;a>=son;a--)
+    {
+        kontrol=0;
+       for(b=2;b<=a/2;b++){
+        if(a%b==0){
+            kontrol=1;}
+        }
+        if(kontrol==0 && a!=0 && a!=1){
+            printf("%d,",a);
+        }
+    }}
+    if(ilk<son){
+    printf("Asal sayilar %d ile %d arasindaki:",ilk,son);
+    for(a=ilk;a<=son;a++)
+    {
+        kontrol=0;
+       for(b=2;b<=a/2;b++){
+        if(a%b==0){
+            kontrol=1;}
+        }
+        if(kontrol==0 && a!=0 && a!=1){
+            printf("%d,",a);
+        }
+    }}
     return 0;
-}
-void xParseSeconds (int seconds)
-{
-    int san,saat,dk;
-    saat=seconds/3600;
-    dk=(seconds-saat*3600)/60;
-    san=(seconds-(saat*3600+dk*60));
-    printf("%d saat %d dakika %d saniye",saat,dk,san);
-
 }
