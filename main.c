4fc98d4aca8af45102d6d48a56b39bd84aa93cdc
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 17:31:27 +0300

    Girilen bilgilere göre en büyük çocuğu bulma
    Kişi sayısı,ad,doğum günü,doğum ayı,doğum yılı girilir.
    Sonra en büyük çocuk bulunur.

--- main.c
+++ main.c
@@ -1,50 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
-#include<ctype.h>
-int AracBilgiAl(char d[][100])
+struct DTarih
 {
-    char *k;
-    int i=0;
-     do
-     {
-         fgets(d[i],100,stdin);
-         fflush(stdin);
-         k=strstr(d[i],"END");
-         i++;
-     }
-    while(k==NULL);
-    for(int j=0;j<i;j++)
-        printf("\n%s",d[j]);
-    return i-1;
-}
-int DogruGirisListele(char d[50][100],int x)
+   int gun;
+   int ay;
+   int yil;
+ };
+struct sahis_bilgileri
 {
-    int i,say=0,gec=0,uretimY;
-    float benzin;
-    char *k,*a,marka[10];
-     for(i=0;i<x;i++)
-       {
-           gec=0;
-         k=strstr(d[i]," ");
-       while(k!=NULL)
+   char Ad[50];
+  struct DTarih dogum_tarihi;
+ };
+ struct sahis_bilgileri Enb(struct sahis_bilgileri x[],int n)
+ {
+    struct sahis_bilgileri a;
+    struct sahis_bilgileri enb;
+    int i,j;
+    enb=x[i];
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
       {
-           gec++;
-          k=strstr(k+1," ");
-       }
-       strcpy(marka,strtok(d[i]," "));
-       uretimY=atoi(strtok(NULL," "));
-       benzin=atof((strtok(NULL," ")));
-       if(gec==2 && uretimY!=0 && benzin!=0.0)
-         printf("%20s %-10d %.1f\n",marka,uretimY,benzin);
+        if(enb.dogum_tarihi.yil>x[j].dogum_tarihi.yil)
+             enb=x[j];
+       else if(enb.dogum_tarihi.yil==x[j].dogum_tarihi.yil && enb.dogum_tarihi.ay>x[j].dogum_tarihi.ay)
+        enb=x[j];
+       else if(enb.dogum_tarihi.yil==enb.dogum_tarihi.yil && enb.dogum_tarihi.ay==x[j].dogum_tarihi.ay && enb.dogum_tarihi.gun>x[j].dogum_tarihi.gun)
+       enb=x[j];
+      }
     }
-
-}
+    return enb;
+ };
 int main()
 {
-     char d[50][100];
-     int i=0;
-     i=AracBilgiAl(d);
-     DogruGirisListele(d,i);
+    int n,i;
+    printf("Kisi sayisini giriniz:");
+    scanf("%d",&n);
+    struct sahis_bilgileri x1[n];
+    struct sahis_bilgileri enB;
+    printf("\nBilgieri giriniz:\n");
+    for(i=0;i<n;i++)
+    {
+        printf("\nAdi:");
+        scanf("%s",&x1[i].Ad);
+        printf("\nDogdugu gun:");
+        scanf("%d",&x1[i].dogum_tarihi.gun);
+        printf("\nDogdugu ay:");
+        scanf("%d",&x1[i].dogum_tarihi.ay);
+        printf("\nDogdugu yil:");
+        scanf("%d",&x1[i].dogum_tarihi.yil);
+    }
+    printf("\n\nKayitlar yazdirilyor...\n");
+    for(i=0;i<n;i++)
+    {
+        printf("Ad:%s\n",x1[i].Ad);
+        printf("Dogum tarihi:%d.%d.%d\n",x1[i].dogum_tarihi.gun,x1[i].dogum_tarihi.ay,x1[i].dogum_tarihi.yil);
+    }
+    enB=Enb(x1,n);
+    printf("\n\nYasi en buyuk kisi:%s %d.%d.%d",enB.Ad,enB.dogum_tarihi.gun,enB.dogum_tarihi.ay,enB.dogum_tarihi.yil);
     return 0;
 }
