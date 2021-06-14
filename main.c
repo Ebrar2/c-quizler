1c9338203483364d93c25098ffacadbf4fb13180
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 16:43:19 +0300

    Satır şekilde stringin uzunluğunu bulup ters çevirme

--- main.c
+++ main.c
@@ -1,57 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<string.h>
 #include<ctype.h>
-#include<math.h>
-void SayiBul(char k[100],int d[100],int *n)
+void print_array(const char arr[][100])
 {
-    int i,say=0,gec,a=0;
-    int top=0;
-    int d2[100]={-1};
-    for(i=0;k[i]!='\0';i++)
-    {
-        a=0;
-        if(isdigit(k[i]))
-        {
-            d[say]=atoi(k+i);
-            gec=d[say];
-            while(gec!=0)
-            {
-                 gec=gec/10;
-                 a++;
-            }
-            i=i+a-1;
-            say++;
-        }
-    }
-    *n=say;
-}
-void SayiSirala(int d[],int n)
-{
-    int i=0,j,gec;
-    for(i=0;i<n;i++)
-    {
-        for(j=i+1;j<n;j++)
-        {
-            if(d[i]<d[j])
-            {
-                gec=d[i];
-                d[i]=d[j];
-                d[j]=gec;
-            }
-        }
-    }
+     int i,j,x=0;
+     for(i=0;i<5;i++)
+     {
+         x=0;
+         for(j=strlen(arr[i])-1;j>-1;j--)
+             {
+                  printf("%c",arr[i][j]);
+             }
+             printf(" uzunluk:%d\n",strlen(arr[i]));
+     }
 }
 int main()
 {
-    char k[100];
-    int i,n;
-    int d[100]={0};
-    printf("Input:");
-    fgets(k,100,stdin);
-    SayiBul(k,d,&n);
-    SayiSirala(d,n);
-    printf("\n\nSiralanmis sayilar:\n");
-    for(i=0;i<n;i++)
-        printf("%d\n",d[i]);
+    int i;
+    char d[5][100]={"Bilgisayar Muhendisligi Bilgisayar Laboratuvari",
+              "Birinci Sinif",
+              "String cumlelerin uzunluk toplamini bulun.",
+              "sureniz yetmis bes dakika",
+              "soruyu dikkatli okuyun!"};
+    printf("Ilk Cumleler:\n");
+   for(i=0;i<5;i++)
+      printf(" '%s'  uzunluk:%d\n",d[i],strlen(d+i));
+     printf("\n\nTers Cevrilmis Hali:\n");
+    print_array(d);
+
+
     return 0;
 }
