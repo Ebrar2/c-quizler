6c48760eccd500648bb1108f3b5f0bb66a3f9869
Ebrar2 <73036186+Ebrar2@users.noreply.github.com>
2021-06-14 16:02:38 +0300

    Rastgele dizi oluşturup sıralama yapıp birde ilk çiftler sonra tekler olarak yeni sıralam oluşturma

--- main.c
+++ main.c
@@ -1,39 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-//EBRAR TEMEL 200201085
-
+#include<time.h>
 int main()
 {
-    int ilk,son,a,b,kontrol=0;
-    printf("Ilk deger:");
-    scanf("%d",&ilk);
-    printf("Son deger:");
-    scanf("%d",&son);
-    if(ilk>son){
-    printf("Asal sayilar %d ile %d arasindaki:",son,ilk);
-    for(a=ilk;a>=son;a--)
+    int d[10],i,j,gecici,c=0,t=0;
+    printf("Rastgele dizi:\n");
+    srand(time(NULL));
+    for(i=0;i<10;i++)
     {
-        kontrol=0;
-       for(b=2;b<=a/2;b++){
-        if(a%b==0){
-            kontrol=1;}
-        }
-        if(kontrol==0 && a!=0 && a!=1){
-            printf("%d,",a);
-        }
-    }}
-    if(ilk<son){
-    printf("Asal sayilar %d ile %d arasindaki:",ilk,son);
-    for(a=ilk;a<=son;a++)
+    d[i]=rand()%101;
+     printf("%d,",d[i]);
+    }
+    for(i=0;i<10;i++)
     {
-        kontrol=0;
-       for(b=2;b<=a/2;b++){
-        if(a%b==0){
-            kontrol=1;}
-        }
-        if(kontrol==0 && a!=0 && a!=1){
-            printf("%d,",a);
-        }
-    }}
+        for(j=i+1;j<10;j++)
+          {
+         if(d[i]>d[j])
+           {
+           gecici=d[j];
+           d[j]=d[i];
+           d[i]=gecici;
+           }
+          }
+    }
+      printf("\n\nSirali dizi:");
+      for(i=0;i<10;i++)
+       {
+       printf("%d,",d[i]);}
+
+       printf("\n\n");
+       printf("Once tek sonra cift sayilar sirali dizi\n");
+    for(i=0;i<10;i++)
+  {
+   if(d[i]%2==0)
+    c++;
+    else
+    t++;
+
+}
+   int cift[c],tek[t];
+    int a=0,b=0;
+   for(i=0;i<10;i++)
+   {
+   if(d[i]%2==0)
+    {
+   cift[a]=d[i];
+   a++;
+  }
+   else
+   {
+   tek[b]=d[i];
+    b++;
+   }
+   }
+  for(int i=0;i<t;i++)
+  printf("%d,",tek[i]);
+   for(int i=0;i<c;i++)
+   printf("%d,",cift[i]);
+
     return 0;
 }
